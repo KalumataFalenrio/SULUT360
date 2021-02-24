0655401af5e86f379642a1ecaf309fad937718b9
kalumfalenrio@gmail.com <kalumfalenrio@gmail.com>
2021-02-24 13:39:05 +0800

    project sulut360

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyArFtsuPPNOUNpGXmTCJlVQy1WBD7lb0tM",
+  authDomain: "sulut360-d18ed.firebaseapp.com",
+  projectId: "sulut360-d18ed",
+  storageBucket: "sulut360-d18ed.appspot.com",
+  messagingSenderId: "326875822669",
+  appId: "1:326875822669:web:2e102a7646818856e8ba04",
+  measurementId: "G-MT7ER129VX"
+};
---
+++ screens/SocialButton.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {Text, TouchableOpacity, View, StyleSheet} from 'react-native';
+import {windowHeight, windowWidth} from '../utils/Dimensions';
+// import { useFonts, Roboto_700Bold} from '@expo-google-fonts/roboto';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+
+
+const SocialButton = ({
+  buttonTitle,
+  btnType,
+  color,
+  backgroundColor,
+  ...rest
+}) => {
+  let bgColor = backgroundColor;
+  return (
+    <TouchableOpacity
+      style={[styles.buttonContainer, {backgroundColor: bgColor}]}
+      {...rest}>
+      <View style={styles.iconWrapper}>
+        <FontAwesome name={btnType} style={styles.icon} size={22} color={color} />
+      </View>
+      <View style={styles.btnTxtWrapper}>
+        <Text style={[styles.buttonText, {color: color}]}>{buttonTitle}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default SocialButton;
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    marginTop: 40,
+    width: '70%',
+    height: windowHeight / 17,
+    left : 60,
+    padding: 15,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    borderRadius: 19,
+  },
+  iconWrapper: {
+    width: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  icon: {
+    fontWeight: 'bold',
+  },
+  btnTxtWrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    // fontFamily: 'Roboto_700Bold',
+  },
+});
