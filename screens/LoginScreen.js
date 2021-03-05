f277720ea5409da4c73530f4c7177995f0b046d3
kalumfalenrio@gmail.com <kalumfalenrio@gmail.com>
2021-03-05 11:58:24 +0800

    skip btn

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -5,101 +5,106 @@ import { SafeAreaView ,View, Text, StyleSheet, Button, Image, TouchableOpacity }
 import firebase from 'firebase';
 import SocialButton from '../screens/SocialButton';
 
-class LoginScreen extends Component {
-  isUserEqual = (googleUser, firebaseUser) => {
-    if (firebaseUser) {
-      var providerData = firebaseUser.providerData;
-      for (var i = 0; i < providerData.length; i++) {
-        if (
-          providerData[i].providerId ===
-            firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
-          providerData[i].uid === googleUser.getBasicProfile().getId()
-        ) {
-          // We don't need to reauth the Firebase connection.
-          return true;
-        }
-      }
-    }
-    return false;
-  };
-  onSignIn = googleUser => {
-    console.log('Google Auth Response', googleUser);
-    // We need to register an Observer on Firebase Auth to make sure auth is initialized.
-    var unsubscribe = firebase.auth().onAuthStateChanged(
-      function(firebaseUser) {
-        unsubscribe();
-        // Check if we are already signed-in Firebase with the correct user.
-        if (!this.isUserEqual(googleUser, firebaseUser)) {
-          // Build Firebase credential with the Google ID token.
-          var credential = firebase.auth.GoogleAuthProvider.credential(
-            googleUser.idToken,
-            googleUser.accessToken
-          );
-          // Sign in with credential from the Google user.
-          firebase
-            .auth()
-            .signInAndRetrieveDataWithCredential(credential)
-            .then(function(result) {
-              console.log('user signed in ');
-              if (result.additionalUserInfo.isNewUser) {
-                firebase
-                  .database()
-                  .ref('/users/' + result.user.uid)
-                  .set({
-                    gmail: result.user.email,
-                    profile_picture: result.additionalUserInfo.profile.picture,
-                    first_name: result.additionalUserInfo.profile.given_name,
-                    last_name: result.additionalUserInfo.profile.family_name,
-                    created_at: Date.now()
-                  })
-                  .then(function(snapshot) {
-                    // console.log('Snapshot', snapshot);
-                  });
-              } else {
-                firebase
-                  .database()
-                  .ref('/users/' + result.user.uid)
-                  .update({
-                    last_logged_in: Date.now()
-                  });
-              }
-            })
-            .catch(function(error) {
-              // Handle Errors here.
-              var errorCode = error.code;
-              var errorMessage = error.message;
-              // The email of the user's account used.
-              var email = error.email;
-              // The firebase.auth.AuthCredential type that was used.
-              var credential = error.credential;
-              // ...
-            });
-        } else {
-          console.log('User already signed-in Firebase.');
-        }
-      }.bind(this)
-    );
-  };
-  signInWithGoogleAsync = async () => {
-    try {
-      const result = await Expo.Google.logInAsync({
-        androidClientId: '326875822669-g0r2rglq3hg91vqjn0o14g9am367h4cj.apps.googleusercontent.com',//YOUR_CLIENT_ID_HERE',
-        behavior: 'web',
-        // iosClientId: '', //enter ios client id
-        scopes: ['profile', 'email']
-      });
 
-      if (result.type === 'success') {
-        this.onSignIn(result);
-        return result.accessToken;
-      } else {
-        return { cancelled: true };
+const LoginScreen = ({navigation}) =>{
+  // class AboutScreen extends Component {
+      const goToabout = () =>{
+        navigation.navigate('about');
       }
-    } catch (e) {
-      return { error: true };
-    }
-  };
-  render() {
+// class LoginScreen extends Component {
+  // isUserEqual = (googleUser, firebaseUser) => {
+  //   if (firebaseUser) {
+  //     var providerData = firebaseUser.providerData;
+  //     for (var i = 0; i < providerData.length; i++) {
+  //       if (
+  //         providerData[i].providerId ===
+  //           firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+  //         providerData[i].uid === googleUser.getBasicProfile().getId()
+  //       ) {
+  //         // We don't need to reauth the Firebase connection.
+  //         return true;
+  //       }
+  //     }
+  //   }
+  //   return false;
+  // };
+  // onSignIn = googleUser => {
+  //   console.log('Google Auth Response', googleUser);
+  //   // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+  //   var unsubscribe = firebase.auth().onAuthStateChanged(
+  //     function(firebaseUser) {
+  //       unsubscribe();
+  //       // Check if we are already signed-in Firebase with the correct user.
+  //       if (!this.isUserEqual(googleUser, firebaseUser)) {
+  //         // Build Firebase credential with the Google ID token.
+  //         var credential = firebase.auth.GoogleAuthProvider.credential(
+  //           googleUser.idToken,
+  //           googleUser.accessToken
+  //         );
+  //         // Sign in with credential from the Google user.
+  //         firebase
+  //           .auth()
+  //           .signInAndRetrieveDataWithCredential(credential)
+  //           .then(function(result) {
+  //             console.log('user signed in ');
+  //             if (result.additionalUserInfo.isNewUser) {
+  //               firebase
+  //                 .database()
+  //                 .ref('/users/' + result.user.uid)
+  //                 .set({
+  //                   gmail: result.user.email,
+  //                   profile_picture: result.additionalUserInfo.profile.picture,
+  //                   first_name: result.additionalUserInfo.profile.given_name,
+  //                   last_name: result.additionalUserInfo.profile.family_name,
+  //                   created_at: Date.now()
+  //                 })
+  //                 .then(function(snapshot) {
+  //                   // console.log('Snapshot', snapshot);
+  //                 });
+  //             } else {
+  //               firebase
+  //                 .database()
+  //                 .ref('/users/' + result.user.uid)
+  //                 .update({
+  //                   last_logged_in: Date.now()
+  //                 });
+  //             }
+  //           })
+  //           .catch(function(error) {
+  //             // Handle Errors here.
+  //             var errorCode = error.code;
+  //             var errorMessage = error.message;
+  //             // The email of the user's account used.
+  //             var email = error.email;
+  //             // The firebase.auth.AuthCredential type that was used.
+  //             var credential = error.credential;
+  //             // ...
+  //           });
+  //       } else {
+  //         console.log('User already signed-in Firebase.');
+  //       }
+  //     }.bind(this)
+  //   );
+  // };
+  // signInWithGoogleAsync = async () => {
+  //   try {
+  //     const result = await Expo.Google.logInAsync({
+  //       androidClientId: '326875822669-g0r2rglq3hg91vqjn0o14g9am367h4cj.apps.googleusercontent.com',//YOUR_CLIENT_ID_HERE',
+  //       behavior: 'web',
+  //       // iosClientId: '', //enter ios client id
+  //       scopes: ['profile', 'email']
+  //     });
+
+  //     if (result.type === 'success') {
+  //       this.onSignIn(result);
+  //       return result.accessToken;
+  //     } else {
+  //       return { cancelled: true };
+  //     }
+  //   } catch (e) {
+  //     return { error: true };
+  //   }
+  // };
     return (
       <View style={styles.container}>
         <View style={styles.header}>
@@ -126,7 +131,7 @@ class LoginScreen extends Component {
           onPress={() => this.signInWithGoogleAsync()}
         /> 
 
-        <TouchableOpacity>
+        <TouchableOpacity onPress={goToabout}>
           <Text style={{top:30, left:170, fontWeight: 'bold', fontSize: 18}}>skip</Text>
         </TouchableOpacity>
           </View>
@@ -145,7 +150,6 @@ class LoginScreen extends Component {
               
     );
   }
-}
 export default LoginScreen;
 const styles = StyleSheet.create({
   container: {
